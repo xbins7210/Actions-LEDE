6cae12b548d7e525307e2ed13d5e4b701c393d9e
xbins7210 <50879216+xbins7210@users.noreply.github.com>
2023-02-13 10:00:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openwrt-packages https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
-echo 'src-git sirpdboy-package https://github.com/sirpdboy/sirpdboy-package.git' >>feeds.conf.default
-echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >>feeds.conf.default
+echo 'src-git openwrt-packages https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git sirpdboy-package https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
